de7a5c3e2f453713717445e359a62bef78fafa0b
deka <andreaacevedo6@gmail.com>
2025-01-09 19:26:03 -0300

    agregue el adaptador de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,22 @@ import { loadEnv } from 'vite';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 const env = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server', // Necesario para APIs y auth
+
+  // Necesario para APIs y auth
+  output: 'server',
+
   vite: {
     define: {
       'process.env.PUBLIC_SUPABASE_URL': JSON.stringify(env.PUBLIC_SUPABASE_URL),
       'process.env.PUBLIC_SUPABASE_ANON_KEY': JSON.stringify(env.PUBLIC_SUPABASE_ANON_KEY)
     }
-  }
+  },
+
+  adapter: vercel()
 });
