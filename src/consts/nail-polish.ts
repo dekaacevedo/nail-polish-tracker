c2429bd93a9669b2067bdf22f5f2ae1ba180d8ee
deka <andreaacevedo6@gmail.com>
2025-03-23 22:50:03 -0300

    add más esmaltes 22.50

--- src/consts/nail-polish.ts
+++ src/consts/nail-polish.ts
@@ -10,6 +10,167 @@ export const NAILPOLISH = [
 
     },
     {
+        id: 74,
+        name: "Top Coat",
+        feature: "quick drying",
+        brand: "opi",
+        collection: "",
+        level: "medio",
+        color: "rgb(255 255 255)"
+
+    },
+    {
+        id: 7896111994061,
+        name: "Ponto de luz",
+        feature: "verniz com particulas radiantes",
+        brand: "impala",
+        collection: "",
+        level: "medio",
+        color: "rgb(226, 221, 174)"
+    },
+    {
+        id: 7896111917596,
+        name: "Sublime",
+        feature: "glitter",
+        brand: "impala",
+        collection: "a cor da sua moda",
+        level: "alto",
+        color: "rgb(224 221 210)"
+
+    },
+    {
+        id: 7896111950289,
+        name: "disco ball",
+        feature: "glitter",
+        brand: "impala",
+        collection: "a cor da sua moda",
+        level: "alto",
+        color: "rgb(194, 204, 207)"
+    },
+    {
+        id: 7896111913925,
+        name: "luar",
+        feature: "transparente",
+        brand: "impala",
+        collection: "a cor da sua moda",
+        level: "alto",
+        color: "rgb(255, 255, 255)"
+    },
+    {
+        id: 7896111926963,
+        name: "lolita",
+        feature: "cremoso",
+        brand: "impala",
+        collection: "a cor da sua moda",
+        level: "alto",
+        color: "rgb(225 225 223)"
+    },
+    {
+        id: 7896111913895,
+        name: "neném perolado",
+        feature: "",
+        brand: "impala",
+        collection: "a cor da sua moda",
+        level: "alto",
+        color: "rgb(221 220 227)"
+    }, 
+    {
+        id: 7896111996362,
+        name: "lavanda pacífica",
+        feature: "com suave fragancia",
+        brand: "impala",
+        collection: "Acalmamente",
+        level: "alto",
+        color: "rgb(205 181 216)"
+
+    },   
+    {
+        id: 7896111996485,
+        name: "ficar na boa",
+        feature: "com suave fragancia",
+        brand: "impala",
+        collection: "Acalmamente",
+        level: "alto",
+        color: "rgb(165, 186, 182)"
+    },
+    {
+        id: 7896111996249,
+        name: "foco no bem estar",
+        feature: "com suave fragancia",
+        brand: "impala",
+        collection: "Acalmamente",
+        level: "alto",
+        color: "rgb(205, 80, 102)"
+    },
+    {
+        id: 7896111999028,
+        name: "Roça Rosa",
+        feature: "cremoso",
+        brand: "impala",
+        collection: "Ana Castela",
+        level: "alto",
+        color: "rgb(161 39 129)"
+
+    },
+    {
+        id: 7896111999189,
+        name: "Sou do mato",
+        feature: "cremoso",
+        brand: "impala",
+        collection: "Ana Castela",
+        level: "alto",
+        color: "rgb(124 127 87)"
+
+    },
+    {
+        id: 7896111999226,
+        name: "solta o agronejo!",
+        feature: "cremoso",
+        brand: "impala",
+        collection: "Ana Castela",
+        level: "alto",
+        color: "rgb(120, 67, 168)"
+    },
+    {
+        id: 7896111999301,
+        name: "estrela do rodeio",
+        feature: "glitter",
+        brand: "impala",
+        collection: "Ana Castela",
+        level: "alto",
+        color: "rgb(208, 207, 203)"
+    },
+    {
+        id: 7891350042160,
+        name: "Regendo minha história",
+        feature: "cremoso",
+        brand: "risque",
+        collection: "Bridgerton",
+        level: "alto",
+        color: "rgb(188 64 116)"
+
+    },
+    {
+        id: 7891350042214,
+        name: "A Autora Revelado",
+        feature: "metálico",
+        brand: "risque",
+        collection: "Bridgerton",
+        level: "alto",
+        color: "rgb(48 25 68)"
+
+    },
+    {
+        id: 7417045429,
+        name: "Black to Black",
+        feature: "",
+        brand: "sally-hansen",
+        collection: "INSTA-DRI",
+        level: "alto",
+        color: "rgb(0 0 0)"
+
+    },
+    {
         id: 998076,
         name: "Potencia máxima",
         feature: "metal like",
@@ -29,36 +190,7 @@ export const NAILPOLISH = [
         color: "rgb(149 53 149)"
 
     },
-    {
-        id: 7417045429,
-        name: "Black to Black",
-        feature: "",
-        brand: "sally-hansen",
-        collection: "INSTA-DRI",
-        level: "alto",
-        color: "rgb(0 0 0)"
-
-    },
-    {
-        id: 7891350042160,
-        name: "Regendo minha história",
-        feature: "cremoso",
-        brand: "risque",
-        collection: "Bridgerton",
-        level: "alto",
-        color: "rgb(188 64 116)"
-
-    },
-    {
-        id: 74,
-        name: "Top Coat",
-        feature: "quick drying",
-        brand: "opi",
-        collection: "",
-        level: "medio",
-        color: "rgb(255 255 255)"
-
-    },
+    
     {
         id: 7896111996850,
         name: "Brasiliana",
@@ -110,16 +242,6 @@ export const NAILPOLISH = [
 
     },
     {
-        id: 7896111917596,
-        name: "Sublime",
-        feature: "glitter",
-        brand: "impala",
-        collection: "a cor da sua moda",
-        level: "alto",
-        color: "rgb(224 221 210)"
-
-    },
-    {
         id: 7896111986905,
         name: "Cheia de Manias",
         feature: "full colors",
@@ -127,36 +249,6 @@ export const NAILPOLISH = [
         collection: "JUPAES",
         level: "alto",
         color: "rgb(133 48 80)"
-
-    },
-    {
-        id: 7896111999028,
-        name: "Roça Rosa",
-        feature: "cremoso",
-        brand: "impala",
-        collection: "Ana Castela",
-        level: "alto",
-        color: "rgb(161 39 129)"
-
-    },
-    {
-        id: 7896111999189,
-        name: "Sou do mato",
-        feature: "cremoso",
-        brand: "impala",
-        collection: "Ana Castela",
-        level: "alto",
-        color: "rgb(124 127 87)"
-
-    },
-    {
-        id: 7896111998618,
-        name: "Faça sua manobra",
-        feature: "cremoso - suplementado",
-        brand: "impala",
-        collection: "Movimenta!",
-        level: "alto",
-        color: "rgb(205 145 176)"
 
     },
     {
@@ -170,24 +262,13 @@ export const NAILPOLISH = [
 
     },
     {
-        id: 7896111996362,
-        name: "lavanda pacífica",
-        feature: "com suave fragancia",
+        id: 7896111998014,
+        name: "Pilote a sua jornada",
+        feature: "metal like",
         brand: "impala",
-        collection: "Acalmamente",
+        collection: "JUPAES",
         level: "alto",
-        color: "rgb(205 181 216)"
-
-    },
-    {
-        id: 7891350042214,
-        name: "A Autora Revelado",
-        feature: "metálico",
-        brand: "risque",
-        collection: "Bridgerton",
-        level: "alto",
-        color: "rgb(48 25 68)"
-
+        color: "rgb(171 180 182)"
     },
     {
         id: 7896111998892,
@@ -199,25 +280,6 @@ export const NAILPOLISH = [
         color: "rgb(212 213 207)"
 
     },
-    {
-        id: 7896111998014,
-        name: "Pilote a sua jornada",
-        feature: "metal like",
-        brand: "impala",
-        collection: "JUPAES",
-        level: "alto",
-        color: "rgb(171 180 182)"
-
-    },
-    {
-        id: 7896111913895,
-        name: "neném perolado",
-        feature: "",
-        brand: "impala",
-        collection: "a cor da sua moda",
-        level: "alto",
-        color: "rgb(221 220 227)"
-    },    
     {
         id: 7896111998779,
         name: "Nadar de braçada",
@@ -235,16 +297,17 @@ export const NAILPOLISH = [
         collection: "Movimenta!",
         level: "alto",
         color: "rgb(134 110 171)"
-    },    
+    }, 
     {
-        id: 7896111926963,
-        name: "lolita",
-        feature: "cremoso",
+        id: 7896111998618,
+        name: "Faça sua manobra",
+        feature: "cremoso - suplementado",
         brand: "impala",
-        collection: "a cor da sua moda",
+        collection: "Movimenta!",
         level: "alto",
-        color: "rgb(225 225 223)"
-    },    
+        color: "rgb(205 145 176)"
+
+    },   
     {
         id: 7896111985793,
         name: "brilhe como diamante",
@@ -255,24 +318,6 @@ export const NAILPOLISH = [
         color: "rgb(142 133 138)"
     },
     {
-        id: 7896111999226,
-        name: "solta o agronejo!",
-        feature: "cremoso",
-        brand: "impala",
-        collection: "Ana Castela",
-        level: "alto",
-        color: "rgb(120, 67, 168)"
-    },
-    {
-        id: 7896111999301,
-        name: "estrela do rodeio",
-        feature: "glitter",
-        brand: "impala",
-        collection: "Ana Castela",
-        level: "alto",
-        color: "rgb(208, 207, 203)"
-    },
-    {
         id: 7896111985830,
         name: "comemore a vida",
         feature: "glitter 3D",
@@ -281,14 +326,16 @@ export const NAILPOLISH = [
         level: "alto",
         color: "rgb(169, 152, 112)"
     },
+    
     {
-        id: 7896111996485,
-        name: "ficar na boa",
-        feature: "com suave fragancia",
+        id: 7896111948859,
+        name: "na mira 3D",
+        feature: "glitter 3D",
         brand: "impala",
-        collection: "Acalmamente",
+        collection: "VIVA, AME & BRILHE",
         level: "alto",
-        color: "rgb(165, 186, 182)"
+        color: "rgb(45, 28, 30)"
     },
+    
     
 ]
